0666b4ae1bbb4440365dfc32ac9db585b23df62b
Shovity-ubuntu <shovity@gmail.com>
2016-05-02 13:56:16 +0700

    Them img va sua anime.index

--- resources/views/animes/index.blade.php
+++ resources/views/animes/index.blade.php
@@ -8,8 +8,23 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-6 col-sm-offset-3">
-				<h2>Ten animes</h2>
-				<img src="" alt="">
+				<h2>Anime name</h2>
+				<img src="{{ asset('img/anime0.jpg') }}" class="img-responsive" alt="Image">
+			</div>
+
+			<div class="col-sm-6 col-sm-offset-3">
+				<h2>Anime name</h2>
+				<img src="{{ asset('img/anime1.jpg') }}" class="img-responsive" alt="Image">
+			</div>
+
+			<div class="col-sm-6 col-sm-offset-3">
+				<h2>Anime name</h2>
+				<img src="{{ asset('img/anime2.jpg') }}" class="img-responsive" alt="Image">
+			</div>
+
+			<div class="col-sm-6 col-sm-offset-3">
+				<h2>Anime name</h2>
+				<img src="{{ asset('img/anime3.jpg') }}" class="img-responsive" alt="Image">
 			</div>
 		</div>
 	</div>
